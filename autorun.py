f349eef3faddd71dd24bbff5b2bd0ce9e0737d40
jjdelcerro <jjdelcerro@administrador-Lenovo-G50-70>
2020-05-06 10:21:34 +0200

    Atrapados errores del autorun para que no intente hacer nada si no estan instalados los plugins de los que depende.

--- autorun.py
+++ autorun.py
@@ -3,13 +3,24 @@ import gvsig
 from gvsig import getResource
 from java.io import File
 from org.gvsig.tools import ToolsLocator
-from addons.AoristicClockGrid.aoristicClockGridGeoprocess import AoristicClockGridGeoprocess
+
+
+try:
+  from addons.AoristicClockGrid.aoristicClockGridGeoprocess import AoristicClockGridGeoprocess
+except:
+  import sys
+  ex = sys.exc_info()[1]
+  gvsig.logger("Can't load module 'AoristicClockGridGeoprocess'. " + str(ex), gvsig.LOGGER_WARN)#, ex)
+  AoristicClockGridGeoprocess = None
+
 
 def i18nRegister():
     i18nManager = ToolsLocator.getI18nManager()
     i18nManager.addResourceFamily("text",File(getResource(__file__,"i18n")))
   
 def main(*args):
+  if AoristicClockGridGeoprocess == None:
+    return
   i18nRegister()
   process = AoristicClockGridGeoprocess()
   process.selfregister("Scripting")
